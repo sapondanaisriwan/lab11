5276e75b6484034de421a487da549f81f82ee00b
Sapondanai Sriwan <sapondanaisriwan@gmail.com>
2024-12-20 16:22:50 +0700

    .

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,18 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+
+  string grade[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+  srand(time(0));
+  int randNum = rand() % 9;
+  cout << "Press Enter 3 times to reveal your future.\n";
+  for (int i = 0; i < 3; i++)
+  {
+    cin.get();
+  }
+  cout << "You will get " << grade[randNum] << " in this 261102.";
+
+  return 0;
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+  // string grade[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+  // int randNum = rand() % 8 + 1;
+  // cout << "Press Enter 3 times to reveal your future.\n";
+  // for (int i = 0; i < 3; i++)
+  // {
+  //   cin.ignore();
+  // }
+  // cout << "You will get " << grade[randNum] << " in this 261102.";
